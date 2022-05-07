180d77f9c12797e1ced008164bc16b0ec2fbf1cd
abhinavr11 <abhinav.raghuvanshi2.ar@gmail.com>
2022-05-08 03:14:48 +0530

    first commit

---
+++ hktv.py
@@ -0,0 +1,84 @@
+import requests
+import urllib.parse
+import sys
+
+
+def scrape():
+    zones = ["thirteenlandmarks","supermarket","personalcarenhealth","beautynhealth","homenfamily","housewares","deals","sportsntravel"]
+    headers = {
+        "accept-encoding":"gzip",
+        "authorization":"Bearer 2de2ea56-4e3c-4401-9a76-c014c70bdc08",
+        "connection":"Keep-Alive",
+        "cookie":"LBI=-1941669348; inapp=true; fence=0; JSESSIONID=7666D5240B540824DE19EFA71540EF49",
+        "device-type":"android",
+        "host":"www.hktvmall.com",
+        "ott-uuid":"97665ca7-880e-43ce-8a11-5429d7a8a934",
+        "user-agent":"okhttp/2.2.0"
+    }
+    
+    head = {
+    "accept-encoding":"gzip",
+    "authorization":"Bearer 2de2ea56-4e3c-4401-9a76-c014c70bdc08",
+    "connection":"Keep-Alive",
+    "cookie":"LBI=-1941669348; inapp=true; fence=0; JSESSIONID=0B05C4127AB75E4CFED31284A948BB10",
+    "device-type":"android",
+    "host":"www.hktvmall.com",
+    "ott-uuid":"e04d5772-cb0f-4356-acd5-96819447eb2d",
+    "user-agent":"okhttp/2.2.0"
+    }
+
+    all_pages = []
+    all_pages_links = []
+    products_page_url_base = "https://www.hktvmall.com/hktvwebservices/v1/hktv/search_products_v2?query="
+
+    for zone in zones:
+        categories = "https://www.hktvmall.com/hktvwebservices/v1/hktv/get_category_directory?lang=en&zone="+zone+"&maxLevel=3"
+        cat = requests.get(categories,headers = headers).json()
+        
+        for category in cat['categories'] :
+
+            if 'subcats' in category.keys():        
+                for subcat1 in category['subcats']:
+
+                    if 'subcats' in subcat1.keys():
+                        for subcat2 in subcat1['subcats']:
+
+                            if 'subcats' in subcat2.keys():                     
+                                for subcat3 in subcat2['subcats']:
+                                    all_pages.append(subcat3)
+
+                            else:
+                                all_pages.append(subcat2)                    
+                    else:
+                        all_pages.append(subcat1)
+            else :
+                all_pages.append(category)
+        
+
+    for pg in all_pages:
+        all_pages_links.append(products_page_url_base+urllib.parse.quote(pg['query'])+"&lang=en&currentPage=0&pageSize=100000&user_id=anonymous")
+
+    all_products_links = []
+    lengthpg = len(all_pages_links)
+    for i,lnk in enumerate(all_pages_links) :
+        multiple_prod_pg = requests.get(lnk,headers=headers).json()
+        
+        for prd in multiple_prod_pg['products']:
+            all_products_links.append(prd)
+            sys.stdout.write(f"\rGenerating Product links %i /{lengthpg}" % i)
+            sys.stdout.flush() 
+            
+    product_base_link = "https://www.hktvmall.com/hktvwebservices/v1/hktv/get_product?product_id="
+    products = []
+    lengthp = len(all_products_links)
+    for i,pr_lnk in enumerate(all_products_links):
+        url = product_base_link+pr_lnk['code']+"&lang=en&no_stock_redirect=true&user_id=anonymous"
+        products.append(requests.get(url,headers = head).json())
+        sys.stdout.write(f"\rScraping product %i /{lengthp}" % i)
+        sys.stdout.flush() 
+
+
+
+
+if __name__ == "__main__":
+    scrape()
